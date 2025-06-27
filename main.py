74579b304c00b5e118b749439c14819067c09009
dmytro <dmytro19022006@gmail.com>
2025-06-27 22:21:24 +0300

    fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from scripts.benchmark_runner import BenchmarkRunner
 from scripts.rectangle_generator import RectangleGenerator
 from scripts.results_saver import ResultsSaver
+from scripts.utils.settings import X_MAX_GLOBAL, X_MIN_GLOBAL, Y_MAX_GLOBAL, Y_MIN_GLOBAL
 
 if __name__ == "__main__":
     runner = BenchmarkRunner("db/KievRegion.db")
-    generator = RectangleGenerator((40, 800000), (30, 600000))
+    generator = RectangleGenerator((X_MIN_GLOBAL, X_MAX_GLOBAL), (Y_MIN_GLOBAL, Y_MAX_GLOBAL))
     saver = ResultsSaver()
 
     for rect in generator.generate_rectangles(count=100, min_size=1000, max_size=50000):
