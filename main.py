25e599000ac0b758aca5868fdb4c372029ebb5b7
dmytro <dmytro19022006@gmail.com>
2025-07-01 00:46:52 +0300

    fix

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def run_benchmarks(benchmark_list, detailed=False, repeat=20):
     for benchmark in benchmark_list:
         benchmark_performer = benchmark(BenchmarkRunner)
 
-        suffix = "_det" if detailed else "_agg" # _dt - detailed; _agg - aggregated
+        suffix = "_det" if detailed else "_agg" # _det - detailed; _agg - aggregated
         benchmark_name = benchmark_performer.get_benchmark_name()
         saver = ResultsSaver(path=f"results/{benchmark_name}{suffix}_res.csv")
 
